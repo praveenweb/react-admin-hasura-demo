eb4bd1e5afe7a99134323cd25a88349b536afa2d
Praveen D <praveen@hasura.io>
2019-01-23 20:25:43 +0530

    demo app

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import hasuraDataProvider from 'ra-data-hasura';
+
+import { PostList, PostEdit, PostCreate, PostShow } from './posts';
+import { UserList } from './users';
+import Dashboard from './Dashboard';
+import authProvider from './authProvider';
+
+const hasuraUrl = 'https://pravstest.herokuapp.com';
+const headers = {}
+const App = () => (
+    <Admin
+        dataProvider={hasuraDataProvider(hasuraUrl, headers)}
+        authProvider={authProvider}
+        dashboard={Dashboard}
+    >
+        <Resource
+            name="posts"
+            icon={PostIcon}
+            list={PostList}
+            edit={PostEdit}
+            create={PostCreate}
+            show={PostShow}
+        />
+        <Resource name="users" icon={UserIcon} list={UserList} />
+        <Resource name="comments" list={ListGuesser} />
+    </Admin>
+);
+export default App;
---
+++ src/posts.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import {
+    Show,
+    ShowButton,
+    SimpleShowLayout,
+    RichTextField,
+    DateField,
+    List,
+    Edit,
+    Create,
+    Datagrid,
+    ReferenceField,
+    ReferenceManyField,
+    TextField,
+    EditButton,
+    DisabledInput,
+    LongTextInput,
+    ReferenceInput,
+    SelectInput,
+    SimpleForm,
+    TextInput,
+    Filter,
+} from 'react-admin';
+
+const PostFilter = props => (
+    <Filter {...props}>
+        <TextInput label="Search" source="q" alwaysOn />
+        <ReferenceInput
+            label="User"
+            source="userId"
+            reference="users"
+            allowEmpty
+        >
+            <SelectInput optionText="name" />
+        </ReferenceInput>
+    </Filter>
+);
+
+export const PostList = props => (
+    <List {...props} filters={<PostFilter />}>
+        <Datagrid>
+            <TextField source="id" />
+            <ReferenceField label="User" source="userId" reference="users">
+                <TextField source="name" />
+            </ReferenceField>
+            <ReferenceManyField
+                label="Comments"
+                reference="comments"
+                target="postId"
+            >
+                <Datagrid>
+                    <DateField source="created_at" />
+                    <TextField source="comment" />
+                    <ReferenceField label="User" source="userId" reference="users">
+                        <TextField source="name" />
+                    </ReferenceField>
+                    <EditButton />
+                </Datagrid>
+            </ReferenceManyField>
+            <TextField source="title" />
+            <EditButton />
+            <ShowButton />
+        </Datagrid>
+    </List>
+);
+
+const PostTitle = ({ record }) => {
+    return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+
+export const PostEdit = props => (
+    <Edit title={<PostTitle />} {...props}>
+        <SimpleForm>
+            <DisabledInput source="id" />
+            <ReferenceInput label="User" source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <LongTextInput source="content" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const PostCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <ReferenceInput label="User" source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <LongTextInput source="content" />
+        </SimpleForm>
+    </Create>
+);
+
+export const PostShow = props => (
+    <Show {...props}>
+        <SimpleShowLayout>
+            <TextField source="title" />
+            <RichTextField source="content" />
+            <DateField label="Publication date" source="created_at" />
+        </SimpleShowLayout>
+    </Show>
+);
